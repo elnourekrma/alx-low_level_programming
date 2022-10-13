fa6a0c47af5e62471d903d615efd2c50b29a1977
elnourekrma <elnourekrma@gmail.com>
2022-10-13 12:10:49 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Description: 'this is bullshit'
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 { puts("\"Programming is like building a multilingual puzzle");
 return (0);
