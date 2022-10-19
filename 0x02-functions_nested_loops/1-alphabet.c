4f8f69b6ba0a3d0b44c30f824a5e96de9d05473d
elnourekrma <elnourekrma@gmail.com>
2022-10-19 08:17:34 -0700

    a

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,11 +6,12 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void print_alphabet(void)
+void main(void)
 {char c;
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
+return (0);
 }
