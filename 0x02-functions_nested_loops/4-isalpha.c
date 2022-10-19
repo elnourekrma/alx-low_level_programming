3932d1fa8db5ee4e5ed10edd143a7b9d5577bc0d
elnourekrma <elnourekrma@gmail.com>
2022-10-19 16:12:46 -0700

    4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _islower - writes string "_purchar"
+ *Description: recive frome funchtion _putchar.c The character to print
+ *@c: is the character
+ *Decription: function that checks is c is lowercase.
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+return (0);
+}
