306fe92e82a09084483d0d3d2a0a1687085d7615
elnourekrma <elnourekrma@gmail.com>
2022-10-19 16:54:08 -0700

    betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,11 +3,13 @@
 /**
  * _abs - writes string "_purchar"
  *Description: return the abslute value of an intger
+ *@n: intger
   * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _abs(int n)
-{if (n > 0)
+{
+if (n > 0)
 return (n);
 else if (n < 0)
 return (-n);
