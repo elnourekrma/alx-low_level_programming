cd6624f369f6a9926a006f5facc82b019c1800b8
elnourekrma <elnourekrma@gmail.com>
2022-10-19 15:57:11 -0700

    3-islower-betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,19 +3,19 @@
 /**
  * _islower - writes string "_purchar"
  *Description: recive frome funchtion _putchar.c The character to print
- * function that checks is c is lowercase.
+ *@c: is the character
+ *Decription: function that checks is c is lowercase.
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _islower(int c)
-{
-int i = 'a';
-for (i = 'a'; i <= 'z'; i++)    
+{int i = 'a';
+for (i = 'a'; i <= 'z'; i++)
 {/* refer int c*/
 if (c == i)
 {
 return (1);
 }
 }
-return (0); 
+return (0);
 }
