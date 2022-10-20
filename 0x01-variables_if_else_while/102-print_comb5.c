63529e0db745c5b6184a9e43726f9461cdb52308
elnourekrma <elnourekrma@gmail.com>
2022-10-20 13:39:10 -0700

    0-main.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: code that print all possible cob of digit
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{int n, m;
+for (n = 0; n <= 9; n++)
+{
+for (m = 0; m <= 9; m++)
+{
+if (n < m)
+{putchar(n + '0');
+putchar(m + '0');
+if (n != 8 || (n == 8 && m != 9))
+{putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
