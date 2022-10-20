6cdd362a1f3bb1b562ca1b98bac96bd755871196
elnourekrma <elnourekrma@gmail.com>
2022-10-20 12:41:41 -0700

    betty

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,20 +4,20 @@
  * @n: number of _ to be printed
  * Return: void
  */
-void print_square(int n) 
-{  
+void print_square(int n)
+{
 int i = 0, ii;
-while (i < n && n > 0)    
+while (i < n && n > 0)
 {      
-ii = 0;      
-while (ii < n)	
-{	  
-_putchar('#');	  
-ii++;	  
-}      
-_putchar('\n');      
-i++;      
+ii = 0;
+while (ii < n)
+{
+_putchar('#');
+ii++;
 }
-if (i == 0)    
+_putchar('\n');
+i++;
+}
+if (i == 0)
 _putchar('\n');
 }
