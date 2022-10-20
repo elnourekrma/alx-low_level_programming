9d1441033c18c1f045dc1049ee26a22e74255a00
elnourekrma <elnourekrma@gmail.com>
2022-10-20 12:29:16 -0700

    isa

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,24 @@
 #include "main.h"
-#include <ctype.h>
 /**
- * print_diagonal - function that print a digonal line
- *Description: recive frome funchtion _putchar.c The character to print
-   *Decription: function that checks is c is lowercase.
-   *@n: length of the line
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * print_diagonal - diagonal lines made of backslashes
+ * @n: number of \ to be printed
+ * Return: void
  */
 void print_diagonal(int n)
-{int i = 0, k;
+{
+int i = 0, ii;
 while (i < n && n > 0)
 {
-k = 0;
-while (k <= i)
+ii = 0;
+while (ii < i)
 {
 _putchar(' ');
-k++;
+ii++;
 }
 _putchar('\\');
 _putchar('\n');
 i++;
 }
-if (i <= 0)
+if (i == 0)
 _putchar('\n');
 }
