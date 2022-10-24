24cb2c118fa0831393ef374326b77ab7981484f1
elnourekrma <elnourekrma@gmail.com>
2022-10-24 12:45:08 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ *puts2 - function that changes intger's value to 98
+ *Description: recive frome funchtion a pointer and return nothing
+ *@str: poiner to int
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts2(char *str)
+{int i = 0;
+while (str[i] != '\0')
+{
+if (str[i] % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
