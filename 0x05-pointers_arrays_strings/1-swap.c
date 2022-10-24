ed83dc90466dc4deac693dd3d70e763005358270
elnourekrma <elnourekrma@gmail.com>
2022-10-24 10:47:31 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,16 @@
 #include "main.h"
 #include <ctype.h>
 /**
- * reset_to_98 - function that changes intger's value to 98
- *Description: recive frome funchtion a pointer and return nothing 
- *@*n: poiner to int
+ *swap_int - function that changes intger's value to 98
+ *Description: recive frome funchtion a pointer and return nothing
+ *@a: poiner to int
+ *@b:poniter to 2nd int
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void reset_to_98(int *n)
-{ 
-*n = 98;
+void swap_int(int *a, int *b)
+{
+*a += *b;
+*b = *a - *b;
+*a = *a - *b;
 }
