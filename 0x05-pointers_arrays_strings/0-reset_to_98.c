b6de278262228c1035790efaf5deadbfc680a913
elnourekrma <elnourekrma@gmail.com>
2022-10-24 10:19:29 -0700

    commit after betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,11 @@
 #include <ctype.h>
 /**
  * reset_to_98 - function that changes intger's value to 98
- *Description: recive frome funchtion a pointer and return nothing 
- *@*n: poiner to int
+ *Description: recive frome funchtion a pointer and return nothing
+ *@n: poiner to int
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void reset_to_98(int *n)
-{ 
-*n = 98;
+{*n = 98;
 }
