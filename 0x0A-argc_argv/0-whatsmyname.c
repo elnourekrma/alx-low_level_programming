b474f3c7c41ec1a80b8f16b6f241fe64426a292c
elnourekrma <elnourekrma@gmail.com>
2022-11-09 09:39:19 -0800

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - starting point of the program
+ * Description: function that should return the name of the program
+ *                  of a string to uppercase.
+ *@argc: count of arguments
+ *@argv: array of arguments
+ * Return: 1 at success
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
