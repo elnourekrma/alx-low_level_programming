0994b1551f439d299df0b9d9ad5ecba476835d07
elnourekrma <elnourekrma@gmail.com>
2022-11-09 09:58:40 -0800

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - starting point of the program
+ * Description: function that should return the number of arrguments passed
+ *@argc: count of arguments
+ *@argv: array of arguments
+ * Return: 1 at success
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+printf("\n");
+return (0);
+}
